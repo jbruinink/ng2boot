88fa1884dee460e661f4725392bbd3520addeaed
Jeroen Bruinink <jeroen.bruinink@gmail.com>
2016-12-10 16:28:07 +0100

    Step 1: Generate a new Spring Boot project

---
+++ src/main/java/com/jdriven/ng2boot/Ng2bootApplication.java
@@ -0,0 +1,12 @@
+package com.jdriven.ng2boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Ng2bootApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Ng2bootApplication.class, args);
+	}
+}
